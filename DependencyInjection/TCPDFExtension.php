8f105420467a4a2aeff378ff0f831d9378207047
VladMeh <vladmeh@gmail.com>
2018-04-27 16:35:15 +0300

    bundle & extension

---
+++ DependencyInjection/TCPDFExtension.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by Alpha-Hydro.
+ * @link http://www.alpha-hydro.com
+ * @author Vladimir Mikhaylov <admin@alpha-hydro.com>
+ * @copyright Copyright (c) 2018, Alpha-Hydro
+ *
+ */
+
+namespace Vladmeh\Bundle\TCPDFBundle\DependencyInjection;
+
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class TCPDFExtension extends Extension
+{
+
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        // TODO: Implement load() method.
+    }
+}
---
+++ TCPDFBundle.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by Alpha-Hydro.
+ * @link http://www.alpha-hydro.com
+ * @author Vladimir Mikhaylov <admin@alpha-hydro.com>
+ * @copyright Copyright (c) 2018, Alpha-Hydro
+ *
+ */
+
+namespace Vladmeh\Bundle\TCPDFBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TCPDFBundle extends Bundle
+{
+
+}
