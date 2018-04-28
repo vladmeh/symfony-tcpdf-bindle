5bbebd118e7dd8d97b2da4c7ea6f9ac07eeb5aba
VladMeh <vladmeh@gmail.com>
2018-04-28 12:20:45 +0300

    add service

--- DependencyInjection/TCPDFExtension.php
+++ DependencyInjection/TCPDFExtension.php
@@ -10,14 +10,26 @@
 namespace Vladmeh\Bundle\TCPDFBundle\DependencyInjection;
 
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class TCPDFExtension extends Extension
 {
 
+    /**
+     * @param array $configs
+     * @param ContainerBuilder $container
+     * @throws \Exception
+     */
     public function load(array $configs, ContainerBuilder $container)
     {
-        // TODO: Implement load() method.
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../Resources/config')
+        );
+        $loader->load('services.yml');
     }
 }
