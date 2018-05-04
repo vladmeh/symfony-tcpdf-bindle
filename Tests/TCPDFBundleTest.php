67d7bfbb8873d13f54bdb574a6a25bb4335ec369
VladMeh <vladmeh@gmail.com>
2018-05-04 17:32:07 +0300

    add free test

---
+++ Tests/TCPDFBundleTest.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by Alpha-Hydro.
+ * @link http://www.alpha-hydro.com
+ * @author Vladimir Mikhaylov <admin@alpha-hydro.com>
+ * @copyright Copyright (c) 2018, Alpha-Hydro
+ *
+ */
+
+namespace Vladmeh\Bundle\TCPDFBundle\Tests;
+
+
+use PHPUnit\Framework\TestCase;
+
+class TCPDFBundleTest extends TestCase
+{
+    /**
+     * @throws \Exception
+     */
+    public function testSomething()
+    {
+        $this->assertTrue(true);
+    }
+}
