fef572f93cb3d9b18dbb769c738958c35d4d4c19
VladMeh <vladmeh@gmail.com>
2018-07-09 14:18:08 +0300

    add Service

---
+++ Service/TcPdfService.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by Alpha-Hydro.
+ * @link http://www.alpha-hydro.com
+ * @author Vladimir Mikhaylov <admin@alpha-hydro.com>
+ * @copyright Copyright (c) 2018, Alpha-Hydro
+ *
+ */
+
+namespace Vladmeh\Bundle\TCPDFBundle\Service;
+
+
+abstract class TcPdfService extends \TCPDF
+{
+    public function __construct()
+    {
+        parent::__construct(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, PDF_UNIT, 'UTF-8', false);
+
+        // set document information
+        $this->SetCreator(PDF_CREATOR);
+
+        // set default monospaced font
+        $this->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+
+        // set margins
+        $this->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+        $this->SetHeaderMargin(PDF_MARGIN_HEADER);
+
+        // set image scale factor
+        $this->setImageScale(PDF_IMAGE_SCALE_RATIO);
+
+        // set default font subsetting mode
+        $this->setFontSubsetting(true);
+    }
+
+    public function Open()
+    {
+        parent::Open();
+    }
+
+    public function Close()
+    {
+        parent::Close();
+    }
+
+    public function Header()
+    {
+        parent::Header();
+    }
+
+    public function Footer()
+    {
+        parent::Footer();
+    }
+}
